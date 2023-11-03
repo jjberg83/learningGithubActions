f4046cd447f617be1735a4bd918e3b66995e6225
Jørund Jenssen Berg <jjberg@gmail.com>
2023-11-03 12:09:17 +0100

    Endring for å se om det skjer noe nytt når jeg pusher

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Prøver å lære Github Actions");
+Console.WriteLine("Lagt til to Worflows nå");
